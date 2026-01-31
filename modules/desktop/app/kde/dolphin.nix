49ab5ca961ddf0c51cbeebe87f54a75e524150d2
kagurazakei <maotsugiri@gmail.com>
2026-01-31 21:27:08 +0630

    🚀 update(noctalia-shell): update dms to noctalia-shell

--- modules/desktop/app/kde/dolphin.nix
+++ modules/desktop/app/kde/dolphin.nix
@@ -2,7 +2,41 @@
   inputs,
   pkgs,
   ...
-}: {
+}: let
+  catppuccin-kde-mocha = pkgs.catppuccin-kde.override {
+    flavour = ["mocha"];
+    accents = ["red"];
+  };
+in {
+  # programs.qtengine = {
+  #   enable = true;
+  #
+  #   config = {
+  #     theme = {
+  #       colorScheme = "${catppuccin-kde-macchiato}/share/color-schemes/CatppuccinMacchiatoMauve.colors";
+  #       iconTheme = "Papirus-Dark";
+  #       style = "Darkly";
+  #
+  #       font = {
+  #         family = "Noto Sans";
+  #         size = 11;
+  #         weight = -1;
+  #       };
+  #
+  #       fontFixed = {
+  #         family = "JetBrainsMono Nerd Font";
+  #         size = 11;
+  #         weight = -1;
+  #       };
+  #     };
+  #
+  #     misc = {
+  #       singleClickActivate = false;
+  #       menusHaveIcons = true;
+  #       shortcutsForContextMenus = true;
+  #     };
+  #   };
+  # };
   hj = {
     packages = with pkgs.kdePackages;
       [
