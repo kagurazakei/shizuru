caea1cd88bba48a89138bf675f7f6d1bdd3a97a0
kagurazakei <maotsugiri@gmail.com>
2026-02-27 18:12:51 +0630

    update: equibop themes and atuin theme

--- modules/programs/atuin.nix
+++ modules/programs/atuin.nix
@@ -5,25 +5,27 @@
       xdg.config.files = {
         "atuin/config.toml".text = ''
           dialect = "us"
-           timezone = "local"
-           auto_sync = true
-           update_check = true
-           sync_frequency = "30m"
-           search_mode = "fuzzy"
-           filter_mode = "global"
-           workspaces = false
-           filter_mode_shell_up_key_binding = "global"
-           search_mode_shell_up_key_binding = "fuzzy"
-           style = "full"
-           show_preview = true
-           enter_accept = true
-           local_timeout = 20
-           [stats]
-           [keys]
-           [sync]
-           records = true
-           [preview]
-           [daemon]
+          timezone = "local"
+          auto_sync = true
+          update_check = true
+          sync_frequency = "30m"
+          search_mode = "fuzzy"
+          filter_mode = "global"
+          workspaces = false
+          filter_mode_shell_up_key_binding = "global"
+          search_mode_shell_up_key_binding = "fuzzy"
+          style = "full"
+          show_preview = true
+          enter_accept = true
+          local_timeout = 20
+          [stats]
+          [keys]
+          [sync]
+          records = true
+          [preview]
+          [daemon]
+          [theme]
+          name = "catppuccin-macchiato-lavender"
         '';
       };
     };
