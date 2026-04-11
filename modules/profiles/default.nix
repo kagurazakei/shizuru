e3fe0121c248f793c98c51a741246c684a23c168
kagurazakei <maotsugiri@gmail.com>
2026-04-11 09:10:00 +0630

    before: merg

--- modules/profiles/default.nix
+++ modules/profiles/default.nix
@@ -25,7 +25,7 @@
       self.azalea.modules.nix
       self.azalea.modules.fish
       self.azalea.modules.direnv
-      self.azalea.modules.tailscale
+      # self.azalea.modules.tailscale
       # hardware
       self.azalea.modules.undetected
       self.azalea.modules.zram
