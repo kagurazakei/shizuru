108ab47daf6182a0e34a41cf77fcd3e2d9bb687a
kagurazakei <maotsugiri@gmail.com>
2026-04-13 14:11:20 +0630

    update: spicetify-nix to latest version

--- modules/programs/spotify.nix
+++ modules/programs/spotify.nix
@@ -1,34 +1,37 @@
 {
-  azalea.modules.spotify =
-    { pkgs, sources, ... }:
-    let
-      spicetify-nix = import sources.spicetify-nix {
-        inherit pkgs;
-      };
-      spicePkgs = spicetify-nix.packages;
-    in
-    {
-      imports = [ spicetify-nix.nixosModules.spicetify ];
-      programs.spicetify = {
-        enable = true;
-        enabledExtensions = with spicePkgs.extensions; [
-          powerBar
-          fullAlbumDate
-          fullAppDisplay
-          listPlaylistsWithSong
-          volumePercentage
-          adblock
-          hidePodcasts
-          beautifulLyrics
-          autoSkipExplicit
-          shuffle # shuffle+ (special characters are sanitized out of extension names)
-        ];
-        enabledCustomApps = with spicePkgs.apps; [
-          lyricsPlus
-          newReleases
-        ];
-        theme = spicePkgs.themes.text;
-        colorScheme = "RosePine";
-      };
+  azalea.modules.spotify = {
+    pkgs,
+    sources,
+    flakeCompat,
+    ...
+  }: let
+    # spicetify-nix = import sources.spicetify-nix {
+    #   inherit pkgs;
+    # };
+    spicetify-nix = (flakeCompat.flakeToNix {src = sources.spicetify-nix;}).defaultNix;
+    spicePkgs = spicetify-nix.legacyPackages.${pkgs.system};
+  in {
+    imports = [spicetify-nix.nixosModules.spicetify];
+    programs.spicetify = {
+      enable = true;
+      enabledExtensions = with spicePkgs.extensions; [
+        powerBar
+        fullAlbumDate
+        fullAppDisplay
+        listPlaylistsWithSong
+        volumePercentage
+        adblock
+        hidePodcasts
+        beautifulLyrics
+        autoSkipExplicit
+        shuffle # shuffle+ (special characters are sanitized out of extension names)
+      ];
+      enabledCustomApps = with spicePkgs.apps; [
+        lyricsPlus
+        newReleases
+      ];
+      theme = spicePkgs.themes.text;
+      colorScheme = "RosePine";
     };
+  };
 }
