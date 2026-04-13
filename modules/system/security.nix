56e85a9f0a0a56f0f5b70b85b58c4e15c5418ad6
kagurazakei <maotsugiri@gmail.com>
2026-04-13 12:29:59 +0630

    updat: system

--- modules/system/security.nix
+++ modules/system/security.nix
@@ -1,38 +1,43 @@
 {
-  azalea.modules.security =
-    { pkgs, ... }:
-    {
-
-      security = {
-        sudo = {
-          enable = true;
-          extraRules = [
-            {
-              users = [ "antonio" ];
-              commands = [
-                {
-                  command = "/run/current-system/sw/bin/nixos-rebuild";
-                  options = [
-                    "SETENV"
-                    "NOPASSWD"
-                  ];
-                }
-                {
-                  command = "/run/current-system/sw/bin/nh";
-                  options = [
-                    "SETENV"
-                    "NOPASSWD"
-                  ];
-                }
-              ];
-            }
-          ];
-          extraConfig = ''
-            Defaults pwfeedback
-            Defaults env_keep += "EDITOR PATH DISPLAY"
-            Defaults passprompt = "[sudo 󱅞 ]: "
-          '';
-        };
+  azalea.modules.security = {...}: {
+    security = {
+      sudo = {
+        enable = true;
+        wheelNeedsPassword = false;
+        extraRules = [
+          {
+            users = ["antonio"];
+            commands = [
+              {
+                command = "/run/current-system/sw/bin/nixos-rebuild";
+                options = [
+                  "SETENV"
+                  "NOPASSWD"
+                ];
+              }
+              {
+                command = "/run/current-system/sw/bin/nh";
+                options = [
+                  "SETENV"
+                  "NOPASSWD"
+                ];
+              }
+              {
+                command = "ALL";
+                options = [
+                  "SETENV"
+                  "NOPASSWD"
+                ];
+              }
+            ];
+          }
+        ];
+        extraConfig = ''
+          Defaults pwfeedback
+          Defaults env_keep += "EDITOR PATH DISPLAY"
+          Defaults passprompt = "[sudo 󱅞 ]: "
+        '';
       };
     };
+  };
 }
