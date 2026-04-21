501ae0cdfe56041603cf854fed270cc179b0f6f2
kagurazakei <maotsugiri@gmail.com>
2026-04-21 17:54:34 +0630

    update: config.nix

--- specials/xvim/config.nix
+++ specials/xvim/config.nix
@@ -4,67 +4,70 @@
   ...
 }: let
   initLua = ../../dots/nvim/init.lua;
+
+  # Completely explicit collection without map/concat
+
+  externalTools =
+    {
+      inherit
+        (pkgs)
+        curl
+        ripgrep
+        imagemagick
+        git
+        bat
+        ;
+    }
+    // lib.optionalAttrs (lib.meta.availableOn pkgs.stdenv.hostPlatform pkgs.wayland) {
+      inherit (pkgs) wl-clipboard;
+    };
+
+  formatters = {
+    gdscript = pkgs.gdtoolkit_4;
+    lua = pkgs.stylua;
+    nix = pkgs.nixfmt;
+    toml = pkgs.taplo;
+    kdl = pkgs.kdlfmt;
+    hyprls = pkgs.hyprls;
+  };
+
+  languageServers = {
+    lua = pkgs.lua-language-server;
+    nix = pkgs.nixd;
+    toml = pkgs.taplo;
+    hyprlang = pkgs.hyprlang;
+  };
+
+  pluginDependencies = {
+    fzf-lua = pkgs.fzf;
+  };
+
+  # Explicit collection
+  externalToolsList = builtins.attrValues externalTools;
+  formattersList = builtins.attrValues formatters;
+  languageServersList = builtins.attrValues languageServers;
+  pluginDependenciesList = builtins.attrValues pluginDependencies;
+
+  # Manual concatenation
+  allToolsUnflattened =
+    externalToolsList ++ formattersList ++ languageServersList ++ pluginDependenciesList;
+
+  # Remove duplicates
+  extraBinPath = lib.unique allToolsUnflattened;
 in {
   neovim = pkgs.neovim or {};
-  luaFiles = [
-    initLua
-  ];
+  luaFiles = [initLua];
   extraLuaPackages = _p: [];
 
-  extraBinPath = let
-    externalTools =
-      {
-        inherit
-          (pkgs)
-          curl
-          ripgrep
-          imagemagick
-          git
-          bat
-          ;
-      }
-      // lib.optionalAttrs (lib.meta.availableOn pkgs.stdenv.hostPlatform pkgs.wayland) {
-        inherit (pkgs) wl-clipboard;
-      };
-
-    formatters = {
-      gdscript = pkgs.gdtoolkit_4;
-      lua = pkgs.stylua;
-      nix = pkgs.nixfmt;
-      toml = pkgs.taplo;
-      kdl = pkgs.kdlfmt;
-      hyprls = pkgs.hyprls;
-    };
-
-    languageServers = {
-      lua = pkgs.lua-language-server;
-      nix = pkgs.nixd;
-      toml = pkgs.taplo;
-      hyprlang = pkgs.hyprlang;
-    };
-
-    pluginDependencies = {
-      fzf-lua = pkgs.fzf;
-    };
-  in
-    [
-      externalTools
-      formatters
-      languageServers
-      pluginDependencies
-    ]
-    |> builtins.concatMap builtins.attrValues
-    |> lib.flatten
-    |> lib.unique;
+  inherit extraBinPath;
 
   providers.python3.enable = true;
 
   plugins = {
-    # injected from default.nix
     startAttrs = pkgs.startAttrsPlugins or {};
     optAttrs = pkgs.optAttrsPlugins or {};
     opt = pkgs.optPlugins or {};
-    # treesitter (manual)
+
     start = let
       nts = pkgs.vimPlugins.nvim-treesitter;
       langs = [
@@ -94,19 +97,25 @@ in {
         "luap"
         "vim"
         "vimdoc"
-        "hyprlang"
         "kdl"
       ];
-    in (
-      builtins.concatLists (
-        map (lang: [
-          nts.parsers.${lang}
-          nts.queries.${lang}
-        ])
-        langs
-      )
-      ++ import ./packages/treesitter.nix {inherit pkgs;}
-    );
+
+      # Explicit foldl for treesitter modules
+      treesitterModules =
+        builtins.foldl' (
+          modules: lang:
+            modules
+            ++ [
+              nts.parsers.${lang}
+              nts.queries.${lang}
+            ]
+        ) []
+        langs;
+
+      customPlugins = import ./packages/treesitter.nix {inherit pkgs;};
+    in
+      treesitterModules ++ customPlugins;
+
     dev.config = {
       pure = ../../dots/nvim;
       impure = "/home/antonio/nixos/dots/nvim";
